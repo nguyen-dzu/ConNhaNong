48c81ce9f87f5a780dd5f9a4d6db3eae782dec53
HangLoc <hangloc312001@gmail.com>
2021-05-25 21:08:57 +0700

    [modify]

--- ConNhaNong/Controllers/ProductController.cs
+++ ConNhaNong/Controllers/ProductController.cs
@@ -99,53 +99,5 @@ namespace ConNhaNong.Controllers
                 return View();
             }
         }
-        [HttpPost]
-        public ActionResult AddCart(string ID_P)
-        {
-            var SL = Request.Form["sale_amount"];
-            if (String.IsNullOrEmpty(SL))
-            {
-                SL = 1.ToString();
-            }
-            User Users = (User)Session["User"];
-            if (Users!=null)
-            {
-                var users = context.Users.Where(s => s.Email.Equals(Users.Email)).FirstOrDefault();
-                if (users != null)
-                {
-                    var cart = context.Carts.Where(s => s.ID.Equals(users.ID)).FirstOrDefault();
-                    if (cart == null)
-                    {
-                        Cart cart_ = new Cart();
-                        cart_.ID = users.ID;
-                        cart_.list = ID_P.ToString();
-                        cart_.amount = SL.ToString();
-                        cart_.ID_cart = users.ID;
-                        context.Carts.Add(cart_);
-                        context.SaveChanges();
-                    }
-                    else
-                    {
-                        cart.list += "," + ID_P.ToString();
-                        cart.amount += "," + SL.ToString();
-                        context.SaveChanges();
-                    }
-                    var cart__ = context.Carts;
-                    return View(cart__);
-                }
-                else
-                {
-                    return RedirectToAction("Login", "Home");
-                }
-            }
-            else
-            {
-                return RedirectToAction("Login", "Home");
-            }
-        }
-        public ActionResult Cart()
-        {
-            return View();
-        }
     }
 }
