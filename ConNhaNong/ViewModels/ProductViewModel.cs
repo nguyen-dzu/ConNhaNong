586eac5951c210336e8e88203d816069d850273d
197PM09417 - Nguyễn Quốc Bảo - K25T-PM <bao.197pm09417@vanlanguni.vn>
2021-05-26 20:24:02 +0700

    [Bao Them gio hang]

---
+++ ConNhaNong/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConNhaNong.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name_Product { get; set; }
+        public int Amount { get; set; }
+        public string Image { get; set; }
+        public double? Total { get; set; }
+    }
+}
