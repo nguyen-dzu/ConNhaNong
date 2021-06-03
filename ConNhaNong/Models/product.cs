926c0dab98ac6d058238b5001d2b8cfdc675d477
HangLoc <hangloc312001@gmail.com>
2021-06-03 15:14:46 +0700

    [loc chỉnh sửa giao diện web]

--- ConNhaNong/Models/product.cs
+++ ConNhaNong/Models/product.cs
@@ -1,4 +1,4 @@
-namespace ConNhaNong.Models
+﻿namespace ConNhaNong.Models
 {
     using System;
     using System.Collections.Generic;
@@ -9,19 +9,30 @@ namespace ConNhaNong.Models
     public partial class product
     {
         [StringLength(30)]
+
         public string ID { get; set; }
 
+        [Display(Name ="Giá")]
+        [DataType(DataType.Text)]
         public double? price { get; set; }
 
+        [Display(Name = "Số lượng còn lại")]
+        [DataType(DataType.Text)]
         public int? amount { get; set; }
 
+        [Display(Name = "Tên sản phẩm")]
+        [DataType(DataType.Text)]
         [StringLength(100)]
         public string name_product { get; set; }
 
         [StringLength(2000)]
         public string file_names { get; set; }
 
+
         [StringLength(1000)]
+        [Display(Name = "Mô tả sản phẩm")]
+        [DataType(DataType.Text, ErrorMessage = "Địa chỉ Email không đúng định dạng")]
+        [Required(ErrorMessage = "Mô tả không được bỏ trống")]
         public string Descriptions { get; set; }
     }
 }
